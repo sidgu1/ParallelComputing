b9a96ef1de436ba0290e4d4c9f119dbc6b4a0d15
sidgu1 <120745004+sidgu1@users.noreply.github.com>
2023-10-24 11:51:35 +0530

    Create parallel.cpp

---
+++ parallel.cpp
@@ -0,0 +1,26 @@
+#include<omp.h>
+#include<iostream>
+#include<cstdlib>
+#include<time.h>
+clock_t s,e;
+using namespace std;
+
+int main(){
+    int n=1e+5;
+    // cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        arr[i]=rand()%10000;
+        // printf("%d ",arr[i]);
+    }
+    int sum=0;
+    s=clock();
+    #pragma omp parallel for reduction(+:sum)
+        for(int i=0;i<n;i++){
+        sum=sum+arr[i];//reduction occurs
+    }
+    e=clock();
+    cout<<"\n"<<sum<<endl;
+    cout<<e-s<<endl;
+    return 0;
+}
